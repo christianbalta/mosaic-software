65aa3294ba2ca8680674654be91ae51eecc81801
Christian Balta <christian.balta@spsglobal.com>
2025-09-04 05:55:00 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.kita-luftibus.ch',
+  vite: {
+    plugins: [tailwindcss()],
+    css: {
+      postcss: {
+        plugins: [],
+      },
+    },
+  },
+  integrations: [sitemap()]
+});
