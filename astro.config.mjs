9a244beb3f3b35ea6787b7532c3c8f3c410b8e2f
Christian Balta <christian.balta@spsglobal.com>
2025-09-08 07:21:01 +0200

    Move components to "sections" directory, update imports, add "Portfolio" section, and update site URL in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.kita-luftibus.ch',
+  site: 'https://www.mosaic-software.ch',
   vite: {
     plugins: [tailwindcss()],
     css: {
